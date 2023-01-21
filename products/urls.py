67dc7de1e05ce5dd2a0fec1115f1601f5f18f39c
rezashahrokhi27 <rezashahrokhi27@gmail.com>
2023-01-21 19:04:36 +0330

    add product model to admin and create product list view templates

---
+++ products/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import ProductList
+
+urlpatterns = [
+    path('', ProductList.as_view(), name='product_list'),
+    ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Product
+
+
+class ProductList(generic.ListView):
+    # model = Product
+    queryset = Product.objects.filter(active=True)
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
