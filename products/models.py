04a929e15c7450475fbc749882317c3c5c2a978c
rezashahrokhi27 <rezashahrokhi27@gmail.com>
2023-02-01 14:15:31 +0330

    add translation to product model with gettext_lazy as _

--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
-
+from django.utils.translation import gettext_lazy as _
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -26,24 +26,24 @@ class ActiveCommentManager(models.Manager):
 
 class Comment(models.Model):
     PRODUCT_STARS = [
-        ('1', 'Very Bad'),
-        ('2', 'Bad'),
-        ('3', 'Normal'),
-        ('4', 'Good'),
-        ('5', 'Perfect'),
+        ('1', _('Very Bad')),
+        ('2', _('Bad')),
+        ('3', _('Normal')),
+        ('4', _('Good')),
+        ('5', _('Perfect')),
 
     ]
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
-    body = models.TextField(verbose_name='Comment Text')
-    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name='Score')
+    body = models.TextField(verbose_name=_('Comment Text'))
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_('Score'))
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
     active = models.BooleanField(default=True)
-    recommend = models.BooleanField(default=True)
+    recommend = models.BooleanField(verbose_name=_('Recommend'),default=True)
 
     # Manager
     objects = models.Manager()
