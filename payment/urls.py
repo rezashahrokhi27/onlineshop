cb4517b9f2a402619bcd133a3e1526cdfc9f0e75
rezashahrokhi27 <rezashahrokhi27@gmail.com>
2023-02-19 16:44:33 +0330

    implement zarinpal sandbox test view

--- payment/urls.py
+++ payment/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import payment_process, payment_callback
+from .views import payment_process, payment_callback, payment_process_sandbox, payment_callback_sandbox
 
 app_name = 'payment'
 
 urlpatterns = [
-    path('process/', payment_process, name='payment_process'),
-    path('callback/', payment_callback, name='payment_callback'),
+    path('process/', payment_process_sandbox, name='payment_process'),
+    path('callback/', payment_callback_sandbox, name='payment_callback'),
     ]
--- payment/views.py
+++ payment/views.py
@@ -64,7 +64,7 @@ def payment_callback(request):
 
         res = requests.post(
             url='https://api.zarinpal.com/pg/v4/payment/verify.json',
-            data=request_data,
+            data=json.dumps(request_data),
             headers=request_header,
         )
 
@@ -89,3 +89,88 @@ def payment_callback(request):
                 return HttpResponse(f'تراکنش ناموفق بود.{error_message}{error_code}')
     else:
         return HttpResponse('تراکنش ناموفق بود.')
+
+
+def payment_process_sandbox(request):
+    # Get order id from session
+    order_id = request.session.get('order_id')
+    # Get the order object
+    order = get_object_or_404(Order, id=order_id)
+
+    toman_total_price = order.get_total_price()
+    rial_total_price = toman_total_price * 10
+
+    zarinpal_request_url = 'https://sandbox.zarinpal.com/pg/rest/WebGate/PaymentRequest.json'
+
+    request_header = {
+        'accept': 'application/json',
+        'content-type': 'application/json',
+    }
+    request_data = {
+        'MerchantID': 'abcABCabcABCabcABCabcABCabcABCabcABC',
+        'Amount': rial_total_price,
+        'Description': f'#{order.id}: {order.user.first_name} {order.user.last_name}',
+        'CallbackURL': request.build_absolute_uri(reverse('payment:payment_callback')),
+    }
+
+    res = requests.post(url=zarinpal_request_url, data=json.dumps(request_data), headers=request_header)
+    print(res)
+    data = res.json()
+    print(data)
+    authority = data['Authority']
+    order.zarinpal_authority = authority
+    order.save()
+
+    if 'errors' not in data:
+        return redirect(f'https://sandbox.zarinpal.com/pg/StartPay/{authority}')
+    else:
+        return HttpResponse('Error from zarinpal')
+
+def payment_callback_sandbox(request):
+    payment_authority = request.GET.get('Authority')
+    payment_status = request.GET.get('Status')
+
+    order = get_object_or_404(Order, zarinpal_authority=payment_authority)
+    toman_total_price = order.get_total_price()
+    rial_total_price = toman_total_price * 10
+
+    if payment_status == 'OK':
+        request_header = {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+        }
+        request_data = {
+            'MerchantID': 'abcABCabcABCabcABCabcABCabcABCabcABC',
+            'Amount': rial_total_price,
+            'Authority': payment_authority,
+        }
+
+        res = requests.post(
+            url='https://sandbox.zarinpal.com/pg/rest/WebGate/PaymentVerification.json',
+            # url = 'https://sandbox.zarinpal.com/pg/v4/payment/verify.json',
+            data=json.dumps(request_data),
+            headers=request_header,
+        )
+        print(res.json())
+        if 'errors' not in res.json():
+            data = res.json()
+            payment_code = data['Status']
+
+            if payment_code == 100:
+                order.is_paid = True
+                order.zarinpal_ref_id = data['RefID']
+                order.zarinpal_data = data
+                order.save()
+
+                return HttpResponse('تراکنش موفقیت آمیز بود.')
+
+            elif payment_code == 101:
+                return HttpResponse('تراکنش موفقیت آمیز بود. البته این تراکنش قبلا ثبت شده است. ')
+
+            else:
+                error_code = res.json()['errors']['code']
+                error_message = res.json()['errors']['message']
+                return HttpResponse(f'تراکنش ناموفق بود.{error_message}{error_code}')
+    else:
+        return HttpResponse('تراکنش ناموفق بود.')
+
